1025b875016777ecfc99e88be82301e816e06c34
Erkihun <erkihunbewket@gmail.com>
2026-05-28 12:50:43 +0300

    frty

--- app/Http/Requests/Admin/UpdateVacancyAnnouncementRequest.php
+++ app/Http/Requests/Admin/UpdateVacancyAnnouncementRequest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\Admin;
 
-use App\Services\SanitizeHtml;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -21,18 +20,9 @@ class UpdateVacancyAnnouncementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'subject' => ['required', 'string', 'max:255'],
-            'content' => ['required', 'string'],
+            'subject'      => ['required', 'string', 'max:255'],
+            'content'      => ['required', 'string'],
             'published_at' => ['nullable', 'date'],
         ];
-    }
-
-    protected function prepareForValidation(): void
-    {
-        if ($this->has('content')) {
-            $this->merge([
-                'content' => app(SanitizeHtml::class)->clean($this->string('content')->toString()),
-            ]);
-        }
     }
 }
--- app/Services/SanitizeHtml.php
+++ app/Services/SanitizeHtml.php
@@ -21,12 +21,56 @@ class SanitizeHtml
 
         $config = HTMLPurifier_Config::createDefault();
         $config->set('Cache.SerializerPath', $cachePath);
-        $config->set('HTML.Allowed', 'p,br,strong,b,em,i,ul,ol,li,a[href|title|target|rel],span');
+
+        // Full rich-text allowlist covering everything TinyMCE can produce
+        $config->set('HTML.Allowed', implode(',', [
+            // Headings
+            'h1[style]', 'h2[style]', 'h3[style]', 'h4[style]', 'h5[style]', 'h6[style]',
+            // Block
+            'p[style]', 'div[style]', 'blockquote[style]', 'pre', 'hr',
+            // Inline text
+            'br', 'strong', 'b', 'em', 'i', 'u', 's', 'del', 'ins',
+            'sub', 'sup', 'code', 'mark', 'small',
+            // Span with style (colours, font-size, etc.)
+            'span[style|class]',
+            // Links
+            'a[href|title|target|rel]',
+            // Lists
+            'ul[style]', 'ol[style|type|start]', 'li[style]',
+            // Tables
+            'table[style|border|width|cellpadding|cellspacing|summary]',
+            'thead[style]', 'tbody[style]', 'tfoot[style]',
+            'tr[style]',
+            'th[style|colspan|rowspan|scope|abbr]',
+            'td[style|colspan|rowspan]',
+            'caption', 'colgroup', 'col[style|span|width]',
+            // Images
+            'img[src|alt|width|height|style|class|title|loading]',
+            // Figure
+            'figure[style|class]', 'figcaption',
+        ]));
+
+        // Allow the CSS properties TinyMCE commonly writes
+        $config->set('CSS.AllowedProperties', [
+            'color', 'background-color', 'background',
+            'text-align', 'text-decoration', 'text-transform', 'text-indent',
+            'font-size', 'font-weight', 'font-style', 'font-family',
+            'line-height', 'letter-spacing',
+            'width', 'height', 'max-width', 'min-width',
+            'padding', 'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
+            'margin', 'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
+            'border', 'border-top', 'border-right', 'border-bottom', 'border-left',
+            'border-width', 'border-style', 'border-color', 'border-collapse', 'border-spacing',
+            'float', 'clear', 'vertical-align', 'display',
+            'list-style-type',
+        ]);
+
         $config->set('URI.AllowedSchemes', [
-            'http' => true,
-            'https' => true,
+            'http'   => true,
+            'https'  => true,
             'mailto' => true,
-            'tel' => true,
+            'tel'    => true,
+            'data'   => true, // needed for embedded images pasted into TinyMCE
         ]);
         $config->set('Attr.AllowedFrameTargets', ['_blank']);
         $config->set('HTML.TargetBlank', true);
